5728c0bacb266fc4d20b4dfe31ddfa9b720bf1da
Zuhaib Ali <ezuhaib@gmail.com>
2014-02-15 23:15:33 +0500

    Created walled garden for beta testing

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,14 +12,14 @@ class User < ActiveRecord::Base
   has_and_belongs_to_many :roles
   has_many :likes, :dependent => :destroy
   has_many :mindlogs, :through => :likes, :source => :likeable, :source_type => 'Mindlog'
-  
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :username, :email, :password, :password_confirmation, :remember_me , :gender , :dob , :body , :country
-  attr_accessor :login
-  attr_accessible :login
+  attr_accessor :login,:testing_key #temporary... restricts signups
+  attr_accessible :login,:testing_key #temporary... restricts signups
   # attr_accessible :title, :body
 
       def self.find_first_by_auth_conditions(warden_conditions)
@@ -34,9 +34,16 @@ class User < ActiveRecord::Base
     validates :username, :uniqueness => {:case_sensitive => false}
     validates_presence_of :username , :gender , :dob , :country
     validates_size_of :body , in: 80...1500
+    validate :check_testing_key
 
     def role?(role)
       self.roles.pluck(:title).include?(role.to_s)
+    end
+
+    def check_testing_key
+      unless [:ocipidi66].include? testing_key
+        errors.add(:testing_key, "must be the one provided by Admin")
+      end
     end
 
 end
