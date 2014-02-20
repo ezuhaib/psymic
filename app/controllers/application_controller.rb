3998eb5e0dfc8d37d76794491801e6f67305436f
Zuhaib Ali <ezuhaib@gmail.com>
2014-02-20 17:52:33 +0500

    Added logic for storing and retreiving user options

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,26 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  layout :layout_by_resource
+  before_filter :authorize_mini_profiler
+
   rescue_from CanCan::AccessDenied do |exception|
   	flash[:error] = "Access Denied"
   	redirect_to root_url
   end
-  before_filter :authorize_mini_profiler
-  	def authorize_mini_profiler
-  		if can? :monitor , :all
-    		Rack::MiniProfiler.authorize_request
-  		end
-	end
+
+	def authorize_mini_profiler
+		if can? :monitor , :all
+  		Rack::MiniProfiler.authorize_request
+		end
+  end
+
+
+  def layout_by_resource
+    if devise_controller? && !current_user
+      "walled_garden"
+    else
+      "application"
+    end
+  end
 
 end
