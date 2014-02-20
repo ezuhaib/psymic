a16ab2f7b123cf5614452fbe7fdbd7ebf79c4c21
Zuhaib Ali <ezuhaib@gmail.com>
2014-02-21 02:17:56 +0500

    Added email notifications for new responses
    - Created a proper 'view single response' page

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,9 +1,17 @@
 class UserMailer < ActionMailer::Base
 
-	default :from => "admin@psymic.com"
+	default :from => "\"Psymic\" <admin@psymic.com>"
 
 	def registration_confirmation(user)
 		@username = user.username
 		mail(:to => user.email, :subject => "Registered")
   	end
+
+	def new_response_on_mindlog(response)
+		@response = response
+  	@mindlog = @response.mindlog
+  	address = @mindlog.user.email
+  	mail(:to => address, :subject => "New response over your mindlog: #{@mindlog.title}")
+  end
+
 end
