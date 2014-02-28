ba10ac6f3aa1adf614592a99bf5ea5121083d202
Zuhaib Ali <ezuhaib@gmail.com>
2014-02-28 15:05:59 +0500

    Javascript assets overhaul
    - Moved vendor assests to vendor directory
    - Included minifed versions wherever possible
    - Now only including /application rather than entire tree
    - very specific scripts now selectively required from views
    - Moved inline scripts to files

---
+++ app/assets/javascripts/channel_edit_selectize.js
@@ -0,0 +1,28 @@
+$(function() {
+
+    $('#channel_all_of_these').selectize({
+   	create: true,
+    hideSelected: true,
+    openOnFocus: false,
+    maxOptions: 3,
+    delimiter: ',',
+    valueField: 'name',
+    labelField: 'name',
+    searchField: 'name',
+
+    load: function(query, callback) {
+        if (!query.length) return callback();
+        $.ajax({
+            url: '/mindlogs/tags.json?q=' + encodeURIComponent(query),
+            type: 'GET',
+            error: function() {
+                callback();
+            },
+            success: function(res) {
+                callback(res);
+            }
+        });
+    }
+});
+
+});
