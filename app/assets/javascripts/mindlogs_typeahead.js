86cbe76bba7769d63d9bd5b4926d288aa052b01f
Zuhaib Ali <ezuhaib@gmail.com>
2014-02-28 15:05:59 +0500

    Javascript assets overhaul
    - Moved vendor assests to vendor directory
    - Included minifed versions wherever possible
    - Now only including /application rather than entire tree
    - very specific scripts now selectively required from views
    - Moved inline scripts to files

---
+++ app/assets/javascripts/mindlogs_typeahead.js
@@ -0,0 +1,30 @@
+var mindlogs = new Bloodhound({
+  datumTokenizer: function(d) { return Bloodhound.tokenizers.whitespace(d.title); },
+  queryTokenizer: Bloodhound.tokenizers.whitespace,
+  remote: "/mindlogs/autocomplete?query=%QUERY"
+});
+
+var tags = new Bloodhound({
+  datumTokenizer: function(d) { return Bloodhound.tokenizers.whitespace(d.name); },
+  queryTokenizer: Bloodhound.tokenizers.whitespace,
+  remote: "/mindlogs/autocomplete_tags?query=%QUERY"
+});
+
+$(document).ready(function(){
+	mindlogs.initialize();
+	tags.initialize();
+
+	$("#mindlog_search").typeahead({
+		minLength: 2,
+		highlight: true,
+		autoselect: true
+	},{
+		name: "mindlog",
+		displayKey: "title",
+		source: mindlogs.ttAdapter()
+	},{
+		name: "tags",
+		displayKey: "name",
+		source: tags.ttAdapter(),
+	});
+})
