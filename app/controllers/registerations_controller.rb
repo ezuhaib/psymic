bd1af1f60d8b70138a95cce9844535544a2a82f9
Zuhaib Ali <ezuhaib@gmail.com>
2014-03-04 22:13:46 +0500

    Bugfix: Permissions for superusers limited
    - Must not use if else logic in authorization
    - Removed custom confirm_email page as the flash message is sufficient

--- app/controllers/registerations_controller.rb
+++
@@ -1,9 +0,0 @@
-class RegistrationsController < Devise::RegistrationsController
-# Overriding Devise Internal Registerations Controller
-
-  protected
-
-    def after_update_path_for(resource)
-      user_path(resource)
-    end
-end
