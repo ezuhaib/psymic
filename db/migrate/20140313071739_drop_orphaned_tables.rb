b384f1edf6651da72e49cb5a3251527fea511188
Zuhaib Ali <ezuhaib@gmail.com>
2014-04-10 00:59:38 +0500

    Fixed migration

--- db/migrate/20140313071739_drop_orphaned_tables.rb
+++ db/migrate/20140313071739_drop_orphaned_tables.rb
@@ -1,7 +1,5 @@
 class DropOrphanedTables < ActiveRecord::Migration
   def up
-  	drop_table :cargo_wiki_articles
-  	drop_table :cargo_wiki_users
   	drop_table :searchjoy_searches
   	drop_table :stasus
   end
